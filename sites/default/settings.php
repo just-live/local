b1c65e76e699412068d7a7a95cdeb40c59bc7e02
Your Name <hamuzi99.ht@hotmail.com>
2017-12-11 10:20:23 +0100

    fix ignore and config location

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -794,4 +794,4 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 $settings['install_profile'] = 'standard';
-$config_directories['sync'] = 'sites/default/custom-sync';
+$config_directories['sync'] = 'config/sync';
